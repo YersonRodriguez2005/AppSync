9822327e170998b920a687d6c3f006744a903323
Yerson Fabian Garzon Rodriguez <141519552+YersonRodriguez2005@users.noreply.github.com>
2026-07-22 17:06:25 -0500

    BD

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,11 +9,12 @@ const { Pool } = pkg;
 
 // Configuración del Pool de conexiones para PostgreSQL
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // database: process.env.DB_NAME,
+  connectionString: process.env.DB_URL_DATABASE,
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
